0ede7a221a9fc39a2c1ee766838473d5582a0465
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-09-07 20:49:48 +0545

    Changes I made too long ago, which I didn't even know until I had to make this push

--- apps/client/src/app/components/posts/Posts.tsx
+++ apps/client/src/app/components/posts/Posts.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
 const Posts = () => {
-    const { isPending, error, data } = useQuery({
+    const { data, status, error } = useQuery({
         queryKey: ["posts"],
         queryFn: async () => {
             const response = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -10,34 +10,29 @@ const Posts = () => {
         }
     });
 
-    let content;
-
-    if (isPending) {
-        content = <div>Loading...</div>
+    if (status === 'pending') {
+        return <div>Loading...</div>
     }
 
-    if (error) {
-        content = <div>Error: {error.message}</div>
+    if (status === 'error') {
+        return <div>Error: {error.message}</div>
     }
 
-    if (data) {
-        content = (
+    // status === 'success'
+    return (
+        <div>
+            <h1>Posts</h1>
             <div>
-                <h1>Posts</h1>
-                <div>
-                    {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-                    {data.map((post: any, index: number) => (
-                        <div key={post.id} className="border p-[1rem] my-[1rem]">
-                            <h2>{index + 1}. {post.title}</h2>
-                            {/* <p>{post.body}</p> */}
-                        </div>
-                    ))}
-                </div>
+                {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+                {data.map((post: any, index: number) => (
+                    <div key={post.id} className="border p-[1rem] my-[1rem]">
+                        <h2>{index + 1}. {post.title}</h2>
+                        {/* <p>{post.body}</p> */}
+                    </div>
+                ))}
             </div>
-        )
-    }
-
-    return content;
-}
+        </div>
+    )
+};
 
 export default Posts;
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // import css
@@ -17,6 +18,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
+
+            <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-left' />
         </QueryClientProvider>
     </React.StrictMode>,
 );
